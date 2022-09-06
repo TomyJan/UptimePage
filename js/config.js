5da5a6e5cfa70030f574cdf22c6c3096078cceda
TomyJan <tomyjan6@gmail.com>
2022-09-06 11:23:34 +0800

    first commit

---
+++ js/config.js
@@ -0,0 +1,19 @@
+// array of Monitor-specific API keys or Read-Only API keys to list all monitors
+// https://uptimerobot.com/dashboard.php#mySettings
+var __apiKeys = [
+    'm785588896-d12c9611dd7e9eaa2d6fe527',  // https://www.tomys.top
+    'm785588900-a4c918a10481260f1078c2fc',  // https://blog.tomys.top
+    'm786064279-8972d9c0e02b0d341c3682c3',  // https://pan.tomys.top
+    'm792620727-e92ae8cb9f7795627e7c1143',  // https://mirror.tomys.top
+    'm786064280-c3cc186650176b51d2a8fff3',  // https://api.tomys.top
+    'm785592236-e16a5be198ee718ee38e2b8f',  // https://go.tomys.top
+    'm785623879-7c5e4bfbb980e2ef1e1b17f2',  // https://mz.tomys.top
+    'm785591207-6d25576ff80f7958e02b7ac2',  // https://cdn.tomys.top
+    'm785589568-b7f911c8eb78dc21a2f9353e',  // https://s.tomys.top
+    'm786064206-997776cb08d84a3d8ff265db',  // https://plugin.tomys.top
+    'm792620756-6b27ba1eb353a98554795125'   // https://cdn.amoe.cc
+
+];
+
+// refresh interval (in seconds)
+var __refresh = 300;
